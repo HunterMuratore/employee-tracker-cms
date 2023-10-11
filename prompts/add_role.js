8f63a8d348d0068ce26aba359a8b953875a71e92
Hunter <muratoreh@gmail.com>
2023-10-11 17:31:00 -0400

    Display all departments, roles and managers in prompts

--- prompts/add_role.js
+++ prompts/add_role.js
@@ -2,34 +2,48 @@ const inquirer = require('inquirer');
 const { db } = require('../db/db');
 
 function addRole(callback) {
-    inquirer
-        .prompt([{
-            name: 'role',
-            message: 'What is the name of the role?'
-        },
-        {
-            name: 'salary',
-            message: 'What is the salary of the role?'
-        },
-        {
-            type: 'list',
-            name: 'department',
-            message: 'Which department does the role belong to?',
-            choices: ['department'] // Show all departments from the db
-        }]).then((answers) => {
-            // Add role to the database
-            const { role, salary, department } = answers;
-            const sql = 'INSERT INTO roles (role, salary, department) VALUES (?, ?, ?)';
-            const params = [role, salary, department];
-      
-            db.query(sql, params, (err, results) => {
-              if (err) return console.error('Error adding role:', err);
-            
-              console.log(`Successfully added ${role} to the database`);
+    db.query('SELECT * FROM departments', (err, departments) => {
+        if (err) throw err;
 
-              callback();
-            });
+        let departmentChoices = [];
+
+        departments.forEach(department => {
+            departmentChoices.push(department);
         });
+    
+        inquirer
+            .prompt([{
+                name: 'role',
+                message: 'What is the name of the role?'
+            },
+            {
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department does the role belong to?',
+                choices: departmentChoices // Show all departments from the db
+            }]).then((answers) => {
+                // Add role to the database
+                const { role, salary, department } = answers;
+
+                const selectedDepartment = departments.find(dep => dep.name === department);
+                const department_id = selectedDepartment.id;
+
+                const sql = 'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)';
+                const params = [role, salary, department_id];
+            
+                db.query(sql, params, (err, results) => {
+                  if (err) return console.error('Error adding role:', err);
+                
+                  console.log(`Successfully added ${role} to the database`);
+
+                  callback();
+                });
+            });
+    });
 }
 
 module.exports = addRole;
